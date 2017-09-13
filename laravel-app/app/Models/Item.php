8d68c8b8c0fedc09f2667dab1f051cc6fa1ad51e
ayuki_j <ajjt66554@gmail.com>
2017-09-13 12:41:47 +0900

    model完成

--- laravel-app/app/Models/Item.php
+++ laravel-app/app/Models/Item.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $primaryKey = 'item_id';
+
+    protected $fillable =['img','title','detail','user_id'];
+
 }
